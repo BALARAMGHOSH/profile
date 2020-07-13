45447bf807897aa38d21d02c644dd88edf6278e2
Balaram Ghosh <balaramg80@gmail.com>
2020-07-13 23:27:41 +0530

    Add files via upload

---
+++ assets/css/style.css
@@ -0,0 +1,93 @@
+@import url('https://fonts.googleapis.com/css2?family=B612:wght@700&display=swap');
+body {
+    margin: 0px;
+}
+
+
+
+#name {
+
+    transform: translate(50%);
+    color: black;
+    font-size: 70px;
+    font-family: 'B612', sans-serif;
+    position: absolute;
+    margin: 250px 250px;
+    text-align: center;
+}
+
+#typewriter {
+    transform: translate(30%,120%);
+	color: black ;
+    position: absolute;
+    font-size: 30px;
+    font-family: 'B612', sans-serif;
+    font-weight: 700;
+    width: 800px;
+    margin: 350px 250px;
+    margin-left: 135px;
+    text-align: center;
+}
+
+/*
+a0d0f8
+ea5c5a
+*/
+
+body {
+  margin: 0;
+  font:normal 75% Arial, Helvetica, sans-serif;
+}
+
+canvas {
+  display: block;
+  vertical-align: bottom;
+}
+
+
+#particles-js {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  background-image: url("");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+}
+
+
+
+.count-particles{
+  background: #000022;
+  position: absolute;
+  top: 48px;
+  left: 0;
+  width: 80px;
+  color: #13E8E9;
+  font-size: .8em;
+  text-align: left;
+  text-indent: 4px;
+  line-height: 14px;
+  padding-bottom: 2px;
+  font-family: Helvetica, Arial, sans-serif;
+  font-weight: bold;
+}
+
+.js-count-particles{
+  font-size: 1.1em;
+}
+
+#stats,
+.count-particles{
+  -webkit-user-select: none;
+}
+
+#stats{
+  border-radius: 3px 3px 0 0;
+  overflow: hidden;
+}
+
+.count-particles{
+  border-radius: 0 0 3px 3px;
+}
